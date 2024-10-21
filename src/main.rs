612410ce201504a2ae50b2bc874adb27a990f96b
Michael Curtis <michael@Macbook-MC.local>
2024-10-21 00:48:20 -0400

    Added cd with no args goes to home

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 //=====================================================================================================================
 // My first small rust program! 
 // It is a simple shell with only a few builtins, I have made a few of these.
-// One in C and another in Zig(Still need to add [*:0] strings, found a way to do it but lost interest in the program)
+// One in C and another in Zig(It Does not execute procs in the fork)
 //=====================================================================================================================
 use std::{
     env::{current_dir, set_current_dir}, fs::{create_dir, create_dir_all}, io::{self, Write}, path::Path, process::Command, rc::Rc
 };
+use dirs::home_dir;
 use colored::*;
 fn main() {
         let mut args: Vec<String> = vec![];
         // start loop
         println!("{} {}", "Welcome to", "Rush!".blue().italic().bold());
+        let home = home_dir().expect("failed to get home dir").display().to_string(); // set the users home dir
         loop {
         println!("{}", current_dir().unwrap().display());
-        print!("{} ", "$>".purple().bold());
+        print!("{} ", "$".purple().bold());
         io::stdout().flush().expect("Flush failed!"); // flush the stdout so the print above appears
         args.clear();   // clear out args vector
         // read in line
@@ -40,8 +42,13 @@ fn main() {
                 println!("{}", pwd.display());
             }
             "cd" => {
-                let path = Path::new(&args[1]);
-                set_current_dir(&path).unwrap_or_else(|err| println!("{} {}", "Error:".red(), Rc::new(err)));
+		if args.len() == 1  {
+		    let path = Path::new(&home);
+                    set_current_dir(&path).unwrap_or_else(|err| println!("{} {}", "Error:".red(), Rc::new(err)));
+		} else {
+                    let path = Path::new(&args[1]);
+                    set_current_dir(&path).unwrap_or_else(|err| println!("{} {}", "Error:".red(), Rc::new(err)));
+		}
             }
             "hostname" => {
                 println!("{:?}", hostname::get().unwrap());
@@ -67,9 +74,9 @@ fn main() {
                     .spawn();
                     // match for error handling!
                     match output {
-                    Ok(mut output) => {let _ = output.wait();},
-                    Err(e) => eprintln!("{} {}", "Error:".red(),e),
-                       }
+			Ok(mut output) => {let _ = output.wait();},
+			Err(e) => eprintln!("{} {}", "Error:".red(),e),
+                    }
              }
            }
             println!(" ");
@@ -78,8 +85,6 @@ fn main() {
 }
 
 fn help_msg() {
-    println!("{} is a simple shell with not very many features", "Rush".blue().bold().italic());
-    println!("The best help I can give is either look at the README or at the source code");
+    println!("{} is a simple shell with very few features", "Rush".blue().bold().italic());
     println!("The only builtin that accepts any flags is {}, which will accept {} to make any parent directories", "mkdir".purple().bold(), "-p".bold().purple());
-    println!("Have fun! And please try and break it!");
 }
